26e7a0c3f17641a59a9bdd371617495c032b50be
silasreis <silas.1992@live.com>
2018-09-20 16:54:24 -0300

    Monetização por recompensa.

--- app/src/main/java/com/monteiro/guessmovie/FilmeActivity.java
+++ app/src/main/java/com/monteiro/guessmovie/FilmeActivity.java
@@ -6,8 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class FilmeActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+public class FilmeActivity extends AppCompatActivity implements RewardedVideoAdListener {
 
     //botões da letra escolhida pelo usuário
     private Button bt_lt1;
@@ -38,6 +45,10 @@ public class FilmeActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     private String[] resposta;
+
+    private RewardedVideoAd mAd;
+    private TextView mText;
+    int moeda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -198,6 +209,16 @@ public class FilmeActivity extends AppCompatActivity {
                 resposta[5] = null;
             }
         });
+
+        mText = (TextView)findViewById(R.id.textView);
+
+        MobileAds.initialize(getApplicationContext(),"ca-app-pub-3940256099942544~3347511713");
+
+        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        mAd.setRewardedVideoAdListener(this);
+
+
+        loadRewardVideoAd();
     }
 
     public void inserirLetra(String letra, String lt_resposta){
@@ -295,4 +316,79 @@ public class FilmeActivity extends AppCompatActivity {
 
         return false;
     }
+
+    private void  loadRewardVideoAd()
+    {
+        if(!mAd.isLoaded())
+        {
+            mAd.loadAd("ca-app-pub-3940256099942544/5224354917", new AdRequest.Builder().build());
+        }
+    }
+
+    public void startVideoAd(View view)
+    {
+        if(mAd.isLoaded())
+        {
+            mAd.show();
+        }
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+
+        loadRewardVideoAd();
+
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+
+        //moeda = 0;
+        mText.setText(("0"+(moeda += 15)));
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        mAd.pause(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mAd.resume(this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mAd.destroy(this);
+        super.onDestroy();
+    }
+
 }
